0565fddba3ae892faced84cfa52eb7cef2f2c865
franeprpic <55486607+franeprpic@users.noreply.github.com>
2019-09-23 14:30:31 +0200

    Add files via upload

---
+++ goto.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	point:
+	cin>>a;
+	while(a!=0)
+	{
+		cin>>a;
+		goto point;
+	
+		if(a=0)
+		{
+			break;
+		}
+		
+
+	}
+
+	return 0;
+}
