f6b7ad340e01f10c1c90297690b379f99f9464bf
franeprpic <55486607+franeprpic@users.noreply.github.com>
2020-03-20 08:20:31 +0100

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	
+	vector<int> prvi;
+	prvi.push_back(3);
+	prvi.push_back(10);
+	prvi.push_back(33);
+	
+	cout<<"Vektori su: ";
+	for(unsigned int i=0;i<prvi.size();i++) {
+		cout<<prvi[i]<< " ";
+	}
+	cout<<endl;
+	if (prvi.empty()) {
+		cout<<"Vektor prvi je prazan.";
+	}
+	else {
+		cout<<"Vektor prvi nije prazan.";
+	}
+	cout<<endl;
+	vector<int> drugi;
+	drugi.push_back(11);
+	drugi.push_back(38);
+	if (prvi.size()!=drugi.size()) {
+		cout<<"Stari vektor nije jednak novom vektoru!";
+	}
+	else {
+		cout<<"Stari vektori jednak je novom vektoru!";
+	}
+	
+	cout<<endl;
+	
+	prvi=drugi;
+	
+	if(prvi.size()!=drugi.size()) {
+		cout<<"Stari vektori nije jedan novom vektoru!";
+	}
+	else {
+		cout<<"Stari vektori jednak je novom vektoru!";
+	}
+	
+	cout<<endl;
+	
+	system("pause");
+	return 0;
+}
+
