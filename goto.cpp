aea044f3f65be75bd489bdf995b82af99cca32f7
franeprpic <55486607+franeprpic@users.noreply.github.com>
2020-03-20 09:07:13 +0100

    Update goto.cpp

--- goto.cpp
+++ goto.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include<string>
 using namespace std;
-int main()
-{
-	int a;
-	point:
-	cin>>a;
-	while(a!=0)
+
+int main() {
+	string str;
+	string zarez=",";
+	int rjeci=1;
+	cout<<"upisite recenicu "<<endl;
+	getline(cin,str);
+	for(int i=0;i!=str.size();i++)
 	{
-		cin>>a;
-		goto point;
-	
-		if(a=0)
-		{
-			break;
+		if(str[i]==' '){rjeci=rjeci+1;
+		
 		}
 		
-
 	}
-
+	cout<<"broj rjeci je "<<rjeci<<endl;
+for(int i=0;i!=str.size();i++)
+	{
+		if(str[i]==' '){
+			str[i]=',';
+			
+		}
+	}
+	
+cout<<str;
+	
 	return 0;
 }
